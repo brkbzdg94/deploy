af3592a852f96d0aa30c82bf4d7e79537e641613
Burak <cixnx@hotmail.com>
2018-09-25 18:48:04 +0300

    new rule 2

--- deploy/deploy/Default.aspx.cs
+++ deploy/deploy/Default.aspx.cs
@@ -16,6 +16,18 @@ namespace deploy
 
             target = -num;  // Noncompliant; target = -3. Is that really what's meant?
             target = +num; // Noncompliant; target = 3
+
+
+            int a;
+
+            
+
+            var s = "foo";
+            var t = "fee fie foe " + s.ToString();  // Noncompliant
+            var someObject = new object();
+            var u = "" + someObject.ToString(); // Noncompliant
+            var v = string.Format("{0}", someObject.ToString()); // Noncompliant
+
         }
         
           
