f9e1fb12c46477c1de325eb8a2a5d8bb05f36821
Burak <cixnx@hotmail.com>
2018-10-02 17:58:26 +0300

    mock test

---
+++ deploy/CustomerServiceTest/UnitTest1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using deploy;
+
+
+namespace CustomerServiceTest
+{
+    [TestClass]
+    public class GetCustomer
+    {
+        [TestMethod]
+        public void Can_GetCustomer()
+        {
+            const int customerId = 1;
+            const string firstName = "Burak";
+            const string lastName = "Bozdağ";
+
+            var repository = new Mock<ICustomerRepository>();
+
+            repository.Setup(m => m.GetCustomer(customerId)).Returns(new Customer { FirstName = firstName, LastName = lastName });
+
+            var service = new CustomerService(repository.Object);
+
+            var customer = service.GetCustomer(customerId);
+
+            Assert.IsTrue(customer != null);
+            Assert.IsTrue(customer.FirstName == firstName);
+            Assert.IsTrue(customer.LastName == lastName);
+
+
+        }
+    }
+}
---
+++ deploy/deploy/CustomerService.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace deploy
+{
+    public class CustomerService
+    {
+        private ICustomerRepository _repository;
+
+        public CustomerService(ICustomerRepository repository)
+        {
+            this._repository = repository;
+        }
+        public Customer GetCustomer(int id)
+        {
+            return _repository.GetCustomer(id);
+        }
+    }
+
+    public interface ICustomerRepository
+    {
+        Customer GetCustomer(int id);
+    }
+
+    public class Customer
+    {
+        public string LastName { get; set; }
+        public string FirstName { get; set; }
+    }
+}
