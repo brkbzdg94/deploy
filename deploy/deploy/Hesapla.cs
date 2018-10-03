cdbe73704a527446338f84f0ddbfda6896278b19
Burak <cixnx@hotmail.com>
2018-10-03 10:31:56 +0300

    test sonar analysis

--- deploy/deploy/Hesapla.cs
+++ deploy/deploy/Hesapla.cs
@@ -7,10 +7,16 @@ namespace deploy
 {
     public class Hesapla
     {
-        
         public int topla(int sayi1, int sayi2)
         {
+            int target = -5;
+            int num = 3;
+
+            target = -num;  // Noncompliant; target = -3. Is that really what's meant?
+            target = +num; // Noncompliant; target = 3
             return sayi1 + sayi2;
         }
+
+        
     }
 }
