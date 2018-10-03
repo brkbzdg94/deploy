17284ba352ee1bd6cd4902ad68cce64c0257cc14
Burak <cixnx@hotmail.com>
2018-10-03 12:20:12 +0300

    test

--- deploy/deploy/Hesapla.cs
+++ deploy/deploy/Hesapla.cs
@@ -9,11 +9,11 @@ namespace deploy
     {
         public int topla(int sayi1, int sayi2)
         {
-            int target = -5;
-            int num = 3;
+            //int target = -5;
+            //int num = 3;
 
-            target = -num;  // Noncompliant; target = -3. Is that really what's meant?
-            target = +num; // Noncompliant; target = 3
+            //target = -num;  // Noncompliant; target = -3. Is that really what's meant?
+            //target = +num; // Noncompliant; target = 3
             return sayi1 + sayi2;
         }
 
