0fc1e0beccab13bcc44b3ee547042ae27de4acef
Burak <cixnx@hotmail.com>
2018-10-09 16:30:36 +0300

    dup

---
+++ deploy/deploy/Adem.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace deploy
+{
+    public class Adem
+    {
+        public int MyProperty { get; set; }
+        public int MyProperty1 { get; set; }
+        public int MyProperty2 { get; set; }
+        public int MyProperty3 { get; set; }
+        public int MyProperty4 { get; set; }
+        public int MyProperty5 { get; set; }
+        public int MyProperty6 { get; set; }
+        public int MyProperty7 { get; set; }
+        public int MyProperty8 { get; set; }
+        public int MyProperty9 { get; set; }
+        public int MyProperty10 { get; set; }
+        public int MyProperty12
+        {
+            get; set;
+        }
+}
