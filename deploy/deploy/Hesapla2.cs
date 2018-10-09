6b6bd584911c68026cfafd44424cb6a707813bb2
Burak <cixnx@hotmail.com>
2018-10-09 12:16:03 +0300

    duplications

--- deploy/deploy/Hesapla2.cs
+++ deploy/deploy/Hesapla2.cs
@@ -5,17 +5,8 @@ using System.Web;
 
 namespace deploy
 {
-    public class Hesapla
+    public class Hesapla2
     {
-        public int topla(int sayi1, int sayi2)
-        {
-            //int target = -5;
-            //int num = 3;
-
-            //target = -num;  // Noncompliant; target = -3. Is that really what's meant?
-            //target = +num; // Noncompliant; target = 3
-            return sayi1 + sayi2;
-        }
         public void YeniCustomer(int id)
         {
             //
@@ -30,6 +21,5 @@ namespace deploy
             int a = 1;
             a = a + 1;
         }
-
     }
 }
