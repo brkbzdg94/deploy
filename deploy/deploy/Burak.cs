10df373b87b883cc9fcfcd767ceacd67cf875ab1
Burak <cixnx@hotmail.com>
2018-10-09 22:54:24 +0300

    burak

--- deploy/deploy/Burak.cs
+++ deploy/deploy/Burak.cs
@@ -8,7 +8,14 @@ namespace deploy
     public class Burak
     {
         public void topla() { int a = 1 + 1; }
-        //public void topla1() { int a = 1 + 1; }
+        public void topla1()
+        {
+            int a = 1 + 1;
+            int a1 = 1 + 1;
+            int a2 = 1 + 1;
+            int a3 = 1 + 1;
+            int a4 = 1 + 1;
+        }
         //public void topla2() { int a = 1 + 1; }
         //public void topla3() { int a = 1 + 1; }
         //public void topla4() { int a = 1 + 1; }
