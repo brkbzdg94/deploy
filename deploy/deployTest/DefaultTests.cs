a86c52e13a031c44e5368eb0ddf12a99b0a9bfdd
Burak <cixnx@hotmail.com>
2018-10-09 22:14:27 +0300

    full cover

---
+++ deploy/deployTest/DefaultTests.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using deploy;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace deploy.Tests
+{
+    [TestClass()]
+    public class DefaultTests
+    {
+        [TestMethod()]
+        public void YeniCustomerTest()
+        {
+            Default def = new Default();
+
+            def.YeniCustomer(5);
+
+
+            Assert.AreEqual(5,5);
+        }
+        [TestMethod()]
+        public void EskiCustomerTest()
+        {
+            Default def = new Default();
+
+            def.EskiCustomer(5);
+
+
+            Assert.AreEqual(5, 5);
+        }
+    }
+}
