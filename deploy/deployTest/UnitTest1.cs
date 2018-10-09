22c0a295aa23a2d2c0a2416029ba3fa175febbea
Burak <cixnx@hotmail.com>
2018-10-09 17:02:51 +0300

    aaaa

--- deploy/deployTest/UnitTest1.cs
+++ deploy/deployTest/UnitTest1.cs
@@ -12,6 +12,8 @@ namespace deployTest
             //Arrange
             deploy.Hesapla hesap = new deploy.Hesapla();
 
+            deploy.Adem aa = new deploy.Adem();
+            aa.topla();
             //Act
             int sonuc = hesap.topla(20, 10);
 
@@ -19,5 +21,18 @@ namespace deployTest
             Assert.AreEqual(30, sonuc);
         }
 
+        [TestMethod]
+        public void testToplab()
+        {
+          
+
+            deploy.Burak bb = new deploy.Burak();
+            bb.topla();
+            //Act
+            int sonuc = 30;
+
+            //Assert
+            Assert.AreEqual(30, sonuc);
+        }
     }
 }
