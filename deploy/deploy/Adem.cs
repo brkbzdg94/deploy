c8c0bb2863e130aa8cdeae36b56452e46f9703d4
Burak <cixnx@hotmail.com>
2018-10-09 16:54:05 +0300

    tt

--- deploy/deploy/Adem.cs
+++ deploy/deploy/Adem.cs
@@ -7,21 +7,7 @@ namespace deploy
 {
     public class Adem
     {
-        public int MyProperty { get; set; }
-        public int MyProperty1 { get; set; }
-        public int MyProperty2 { get; set; }
-        public int MyProperty3 { get; set; }
-        public int MyProperty4 { get; set; }
-        public int MyProperty5 { get; set; }
-        public int MyProperty6 { get; set; }
-        public int MyProperty7 { get; set; }
-        public int MyProperty8 { get; set; }
-        public int MyProperty9 { get; set; }
-        public int MyProperty10 { get; set; }
-        public int MyProperty11 { get; set; }
-        public int MyProperty112 { get; set; }
-        public int MyProperty122 { get; set; }
-        public int MyProperty132 { get; set; }
+
 
         public void topla() { int a = 1 + 1; }
         public void topla1() { int a = 1 + 1; }
@@ -29,6 +15,12 @@ namespace deploy
         public void topla3() { int a = 1 + 1; }
         public void topla4() { int a = 1 + 1; }
         public void topla5() { int a = 1 + 1; }
+        public void topla6() { int a = 1 + 1; }
+        public void topla7() { int a = 1 + 1; }
+        public void topla8() { int a = 1 + 1; }
+        public void topla9() { int a = 1 + 1; }
+        public void topla10() { int a = 1 + 1; }
+        public void topla11() { int a = 1 + 1; }
 
     }
 }
--- deploy/deploy/Burak.cs
+++ deploy/deploy/Burak.cs
@@ -7,29 +7,18 @@ namespace deploy
 {
     public class Burak
     {
-        public int MyProperty { get; set; }
-        public int MyProperty1 { get; set; }
-        public int MyProperty2 { get; set; }
-        public int MyProperty3 { get; set; }
-        public int MyProperty4 { get; set; }
-        public int MyProperty5 { get; set; }
-        public int MyProperty6 { get; set; }
-        public int MyProperty7 { get; set; }
-        public int MyProperty8 { get; set; }
-        public int MyProperty9 { get; set; }
-        public int MyProperty10 { get; set; }
-        public int MyProperty11 { get; set; }
-        public int MyProperty142 { get; set; }
-        public int MyProperty152 { get; set; }
-        public int MyProperty162 { get; set; }
-    
-
-        public void topla() {  int a = 1 + 1; }
+        public void topla() { int a = 1 + 1; }
         public void topla1() { int a = 1 + 1; }
         public void topla2() { int a = 1 + 1; }
         public void topla3() { int a = 1 + 1; }
         public void topla4() { int a = 1 + 1; }
         public void topla5() { int a = 1 + 1; }
+        public void topla6() { int a = 1 + 1; }
+        public void topla7() { int a = 1 + 1; }
+        public void topla8() { int a = 1 + 1; }
+        public void topla9() { int a = 1 + 1; }
+        public void topla10() { int a = 1 + 1; }
+        public void topla11() { int a = 1 + 1; }
 
     }
 }
