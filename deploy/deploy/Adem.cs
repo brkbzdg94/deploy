32a9101d1e89cc68191a55c5b1aea115bd9f9f2a
Burak <cixnx@hotmail.com>
2018-10-09 16:36:50 +0300

    dup6

--- deploy/deploy/Adem.cs
+++ deploy/deploy/Adem.cs
@@ -11,13 +11,7 @@ namespace deploy
         public int MyProperty1 { get; set; }
         public int MyProperty2 { get; set; }
         public int MyProperty3 { get; set; }
-        public int MyProperty4 { get; set; }
-        public int MyProperty5 { get; set; }
-        public int MyProperty6 { get; set; }
-        public int MyProperty7 { get; set; }
-        public int MyProperty8 { get; set; }
-        public int MyProperty9 { get; set; }
-        public int MyProperty10 { get; set; }
-        public int MyProperty12 { get; set; }
+      
+       
     }
 }
