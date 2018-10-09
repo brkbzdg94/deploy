821b2dd665210b8d5dd1ce3bfefbde9af7aad6d1
Burak <cixnx@hotmail.com>
2018-10-09 16:43:24 +0300

    dd

--- deploy/deploy/Adem.cs
+++ deploy/deploy/Adem.cs
@@ -11,7 +11,19 @@ namespace deploy
         public int MyProperty1 { get; set; }
         public int MyProperty2 { get; set; }
         public int MyProperty3 { get; set; }
-      
-       
+        public int MyProperty4 { get; set; }
+        public int MyProperty5 { get; set; }
+        public int MyProperty6 { get; set; }
+        public int MyProperty7 { get; set; }
+        public int MyProperty8 { get; set; }
+        public int MyProperty9 { get; set; }
+        public int MyProperty10 { get; set; }
+        public int MyProperty12 { get; set; }
+        public int MyProperty112 { get; set; }
+        public int MyProperty122 { get; set; }
+        public int MyProperty132 { get; set; }
+
+
+
     }
 }
--- deploy/deploy/Burak.cs
+++ deploy/deploy/Burak.cs
@@ -11,6 +11,17 @@ namespace deploy
         public int MyProperty1 { get; set; }
         public int MyProperty2 { get; set; }
         public int MyProperty3 { get; set; }
+        public int MyProperty4 { get; set; }
+        public int MyProperty5 { get; set; }
+        public int MyProperty6 { get; set; }
+        public int MyProperty7 { get; set; }
+        public int MyProperty8 { get; set; }
+        public int MyProperty9 { get; set; }
+        public int MyProperty10 { get; set; }
+        public int MyProperty11 { get; set; }
+        public int MyProperty142 { get; set; }
+        public int MyProperty152 { get; set; }
+        public int MyProperty162 { get; set; }
 
     }
 }
